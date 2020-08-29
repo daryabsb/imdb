c617bf915e5165b4e9a377533597fd1fd1cb6243
daryabsb <daryabsb@gmail.com>
2020-08-29 17:11:01 +0300

    Added the templates files and statics

--- app/settings.py
+++ app/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = "app.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -124,6 +124,5 @@ STATIC_URL = "/static/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
-print(MEDIA_URL)
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
--- movie/views.py
+++ movie/views.py
@@ -4,3 +4,12 @@ from core.models import Movie
 
 from django.views.generic import ListView, DetailView
 
+
+class MovieList(ListView):
+    model = Movie
+    template_name = "movies/movie_list.html"
+
+
+class MovieDetail(DetailView):
+    model = Movie
+    template_name = "movies/movie_detail.html"
